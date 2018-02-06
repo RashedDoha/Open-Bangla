579429bc373b6201ee9a78de472b982b41d606e2
Rashed Doha <RashedDoha92@gmail.com>
2018-02-07 03:20:39 +0600

    Added smooth scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,12 +22,38 @@ $(document).ready(function() {
 
     });
 
-
-
-
-
-
-
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]').not('[href="#0"]').click(function(event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 
 });
 
@@ -37,30 +63,25 @@ function setUpHighLights() {
         $learn.css('border-bottom', '2px solid white');
     });
 
-
     $('#learn .section-body').on('scrollSpy:enter', function() {
         removeAllHighlights();
         $learn.css('border-bottom', '2px solid white');
     });
-
 
     $('#dataset').on('scrollSpy:enter', function() {
         removeAllHighlights();
         $dataset.css('border-bottom', '2px solid white');
     });
 
-
     $('#contribute').on('scrollSpy:enter', function() {
         removeAllHighlights();
         $contribute.css('border-bottom', '2px solid white');
     });
 
-
     $('#affiliate').on('scrollSpy:enter', function() {
         removeAllHighlights();
         $affiliate.css('border-bottom', '2px solid white');
     });
-
 
     $('#sketchboard').scrollSpy();
     $('#learn .section-body').scrollSpy();
